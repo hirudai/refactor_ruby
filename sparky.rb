8c07e3c605c4621ded2ee96ae9fade7ede2030d8
daigo.hiruma <daigo.hiruma@GX1845-2.local>
2018-11-24 08:12:41 +0900

    XMLの構造を不十分なコメントだけではなく、メソッド名でわかるように、また、XML文書内に適切なコメントが入るように調整

--- sparky.rb
+++ sparky.rb
@@ -1,7 +1,5 @@
 require './svg'
 
-NUMBER_OF_TOSSES = 1000
-
 def toss
   2 * (rand(2)*2 - 1)
 end
@@ -15,22 +13,33 @@ end
 SQUARE_SIDE = 4
 SPARK_COLOR = 'red'
 
+def sparkline(points)
+  "<!-- sparkline -->
+  #{SVG.polyline(points, 'none', '#333', 1)}"
+end
+
 def spark(centre_x, centre_y, value)
-  "#{SVG.rect(centre_x-(SQUARE_SIDE/2), centre_y-(SQUARE_SIDE/2),
+  "<!-- spark -->
+  #{SVG.rect(centre_x-(SQUARE_SIDE/2), centre_y-(SQUARE_SIDE/2),
           SQUARE_SIDE, SQUARE_SIDE, SPARK_COLOR, 'none', 0) }
+  <!-- final value -->
   #{SVG.text(centre_x+6, centre_y+4, value, 'Verdana', 9, SPARK_COLOR)}"
 end
 
-tosses = values(NUMBER_OF_TOSSES)
+def x_axis(points)
+  "<!-- x-axis -->
+  #{SVG.line(0, 200, points.length, 200, '#999', 1)}"
+end
+
+tosses = values(1000)
 points = []
 tosses.each_index { |i| points << "#{i},#{200-tosses[i]}" }
 
 data = %Q{<svg xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink" >
-    <!-- x-axis -->
-    #{SVG.line(0, 200, NUMBER_OF_TOSSES, 200, '#999', 1)}
-    #{SVG.polyline(points, 'none', '#333', 1)}
-    #{spark(NUMBER_OF_TOSSES-1, 200-tosses[-1], tosses[-1])}
+    #{x_axis(points)}
+    #{spark_line(points)}
+    #{spark(tosses.length-1, 200-tosses[-1], tosses[-1])}
 </svg>}
 
 #puts "Content-Type:image/svg+xml
