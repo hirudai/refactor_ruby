67545e00821bda00c37e17d0ca17a417cfa337e5
daigo.hiruma <daigo.hiruma@GX1845-2.local>
2018-11-24 00:17:24 +0900

    【欲張りなメソッド】を修正 SVG出力メソッド内に、スパークラインのための固有値が入ってしまい、 SVG出力、スパークラインの表示方法の両方の仕事を持っているため、 それを分離した

--- sparky.rb
+++ sparky.rb
@@ -10,31 +10,35 @@ def values(n)
   a
 end
 
-def rect(centre_x, centre_y)
-  %Q{<rect x="#{centre_x-2}" y="#{centre_y-2}"
-  width="4" height="4"
-  fill="red" stroke="none" stroke-width="0" />}
+def rect(centre_x, centre_y, width, height, fill, stroke, stroke_width)
+  %Q{<rect x="#{centre_x}" y="#{centre_y}"
+  width="#{width}" height="#{height}"
+  fill="#{fill}" stroke="#{stroke}" stroke-width="#{stroke_width}" />}
 end
 
-def text(x, y, msg)
+def text(x, y, msg, font_family, font_size, fill)
   %Q{<text x="#{x}" y="#{y}" 
-    font-family="Verdana" font-size="9"
-    fill="red" >#{msg}</text>}
+    font-family="#{font_family}" font-size="#{font_size}"
+    fill="#{fill}" >#{msg}</text>}
 end
 
-def line(x1, y1, x2, y2)
+def line(x1, y1, x2, y2, stroke, stroke_width)
   %Q{<line x1="#{x1}" y1="#{y1}" x2="#{x2}" y2="#{y2}"
-    stroke="#999" stroke-width="1" />}
+    stroke="#{stroke}" stroke-width="#{stroke_width}" />}
 end
 
-def polyline(points)
-  %Q{<polyline fill="none" stroke="#333" stroke-width="1"
+def polyline(points, fill, stroke, stroke_width)
+  %Q{<polyline fill="#{fill}" stroke="#{stroke}" stroke-width="#{stroke_width}"
     points = "#{points.join(' ')}" />"}
 end
 
+SQUARE_SIDE = 4
+SPARK_COLOR = 'red'
+
 def spark(centre_x, centre_y, value)
-  "#{rect(centre_x, centre_y)}
-  #{text(centre_x+6, centre_y+4, value)}"
+  "#{rect(centre_x-(SQUARE_SIDE/2), centre_y-(SQUARE_SIDE/2),
+          SQUARE_SIDE, SQUARE_SIDE, SPARK_COLOR, 'none', 0) }
+  #{text(centre_x+6, centre_y+4, value, 'Verdana', 9, SPARK_COLOR)}"
 end
 
 tosses = values(NUMBER_OF_TOSSES)
@@ -44,8 +48,8 @@ tosses.each_index { |i| points << "#{i},#{200-tosses[i]}" }
 data = %Q{<svg xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink" >
     <!-- x-axis -->
-    #{line(0, 200, NUMBER_OF_TOSSES, 200)}
-    #{polyline(points)}
+    #{line(0, 200, NUMBER_OF_TOSSES, 200, '#999', 1)}
+    #{polyline(points, 'none', '#333', 1)}
     #{spark(NUMBER_OF_TOSSES-1, 200-tosses[-1], tosses[-1])}
 </svg>}
 
