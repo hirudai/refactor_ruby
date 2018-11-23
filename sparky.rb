6975c7059148f9cf63bc4ca5a49acdb90844a21e
daigo.hiruma <daigo.hiruma@GX1845-2.local>
2018-11-23 23:37:25 +0900

    グローバル変数を使用しない、xml構築文字列をメソッド化

--- sparky.rb
+++ sparky.rb
@@ -10,28 +10,44 @@ def values(n)
   a
 end
 
-def spark(centre_x, centre_y, value)
-  "<rect x=\"#{centre_x-2}\" y=\"#{centre_y-2}\"
-  width=\"4\" height=\"4\"
-  fill=\"red\" stroke=\"none\" stroke-width=\"0\" />
-  <text x=\"#{centre_x+6}\" y=\"#{centre_y+4}\"
-  font-family=\"Verdana\" font-size=\"9\"
-  fill=\"red\" > #{value}</text>"
+def rect(centre_x, centre_y)
+  %Q{<rect x="#{centre_x-2}" y="#{centre_y-2}"
+  width="4" height="4"
+  fill="red" stroke="none" stroke-width="0" />}
 end
 
-$tosses = values(NUMBER_OF_TOSSES)
-points = []
-$tosses.each_index { |i| points << "#{i},#{200-$tosses[i]}" }
+def text(x, y, msg)
+  %Q{<text x="#{x}" y="#{y}" 
+    font-family="Verdana" font-size="9"
+    fill="red" >#{msg}</text>}
+end
 
-data = "<svg xmlns=\"http://www.w3.org/2000/svg\"
-  xmlns:xlink=\"http://www.w3.org/1999/xlink\" >
-  <!-- x-axis -->
-  <line x1=\"0\" y1=\"200\" x2 =\"#{NUMBER_OF_TOSSES}\" y2=\"200\"
-    stroke=\"#999\" stroke-width=\"1\" />
-  <polyline fill=\"none\" stroke=\"#333\" stroke-width=\"1\"
-    points = \"#{points.join(' ')}\" />
-  #{spark(NUMBER_OF_TOSSES-1, 200-$tosses[-1],  $tosses[-1])}
-</svg>"
+def line(x1, y1, x2, y2)
+  %Q{<line x1="#{x1}" y1="#{y1}" x2="#{x2}" y2="#{y2}"
+    stroke="#999" stroke-width="1" />}
+end
+
+def polyline(points)
+  %Q{<polyline fill="none" stroke="#333" stroke-width="1"
+    points = "#{points.join(' ')}" />"}
+end
+
+def spark(centre_x, centre_y, value)
+  "#{rect(centre_x, centre_y)}
+  #{text(centre_x+6, centre_y+4, value)}"
+end
+
+tosses = values(NUMBER_OF_TOSSES)
+points = []
+tosses.each_index { |i| points << "#{i},#{200-tosses[i]}" }
+
+data = %Q{<svg xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink" >
+    <!-- x-axis -->
+    #{line(0, 200, NUMBER_OF_TOSSES, 200)}
+    #{polyline(points)}
+    #{spark(NUMBER_OF_TOSSES-1, 200-tosses[-1], tosses[-1])}
+</svg>}
 
 puts "Content-Type:image/svg+xml
 Content-Length:#{data.length}
